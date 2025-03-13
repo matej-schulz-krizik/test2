a58c2052f20a11f81bf3a2cbf0f1c68cd5684ee6
matej.schulz <matej.schulz@skolakrizik.cz>
2025-03-13 11:03:24 +0100

    finalni na test

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.test;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spinnerOperation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinnerOperation = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.znamenko,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item
+        );
+
+        spinnerOperation.setAdapter(adapter);
 
 
     }
@@ -37,24 +51,72 @@ public class MainActivity extends AppCompatActivity {
         EditText c = (EditText)findViewById(R.id.editTextNumberC);
         int cislo3 = Integer.parseInt(c.getText().toString());
 
+        String znak = spinnerOperation.getSelectedItem().toString();
 
         double vysledek1;
         double vysledek2;
 
+        if (cislo1==0){
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("je to linearni nerovnice"));
+            return;
+
+
+        }
+
 
         int D = (cislo2*cislo2)-4*cislo1*cislo3;
 
-        if (D<=0){
+       /* if (D<=0){
             TextView textView = (TextView) findViewById(R.id.textView);
             textView.setText(String.valueOf("Diksriminant je mensi nebo roven 0"));
             return;
+        } */
+
+        if(D>0&&cislo1>0){
+            vysledek1 = (-cislo2+Math.sqrt(D))/2*cislo1;
+            vysledek2 = (-cislo2-Math.sqrt(D))/2*cislo1;
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("(-inf; " + vysledek1 + ") U (" + vysledek2 + "; +inf)"));
+            
+        } else if (D>0&&cislo1<0) {
+
+            vysledek1 = (-cislo2+Math.sqrt(D))/2*cislo1;
+            vysledek2 = (-cislo2-Math.sqrt(D))/2*cislo1;
+            
+            
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("(" + vysledek1 + "; " + vysledek2 + ")"));
+            
+        } else if (D==0&&cislo1>0) {
+
+            vysledek1 = (-cislo2)/2*cislo1;
+
+            //x se rovnaji
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf(vysledek1));
+
+        } else if (D==0&&cislo1<0) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("prazdna mnozina"));
+        } else if (D<0&&cislo1>0) {
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("(-inf; +inf)"));
+
+
+        } else if (D<0&&cislo1<0) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText(String.valueOf("prazdna mnozina"));
         }
 
-        vysledek1 = (-cislo2+Math.sqrt(D))/2*cislo1;
+
+      /*  vysledek1 = (-cislo2+Math.sqrt(D))/2*cislo1;
         vysledek2 = (-cislo2-Math.sqrt(D))/2*cislo1;
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(String.valueOf("x1= " + vysledek1 + ",x2= " + vysledek2));
+        textView.setText(String.valueOf("x1= " + vysledek1 + ",x2= " + vysledek2));*/
 
     }
 }
